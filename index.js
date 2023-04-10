e5504b57e9fd071e1d476956b12fdbe698696ddf
Victoria <92218796+Highgradecode@users.noreply.github.com>
2023-04-10 19:31:29 +0300

    Done

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ let num2 = 2;
 
 document.getElementById("num1-el").textContent = num1;
 document.getElementById("num2-el").textContent = num2;
+
+const sumEl = document.getElementById("sum-el");
+
+function add() {
+    let result = num1 + num2;
+    sumEl.textContent = "Sum: " + result;
+}
+
+function subtract() {
+    let result = num1 - num2;
+    sumEl.textContent = "Sum: " + result;
+}
+
+function divide() {
+    let result = num1 / num2;
+    sumEl.textContent = "Sum: " + result;
+}
+
+function multiply() {
+    let result = num1 * num2;
+    sumEl.textContent = "Sum: " + result;
+}
